0be641c86cffbd50c388987583044b302bf6d9fe
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 15:49:51 +0200

    Display props

--- src/components/JobPostingCards.vue
+++ src/components/JobPostingCards.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-
+  <div class="text-center d-flex justify-content-center">
+    <b-card-group>
+      <b-card :title="jobPosting.companyName" style="max-width: 20rem;">
+        <b-card-text
+          ><span class="font-weight-bold">Start Date:</span>
+          {{ jobPosting.startDate }}
+          <span class="font-weight-bold">End Date:</span>
+          {{ jobPosting.endDate }}
+        </b-card-text>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 <script>
+import { mapState } from "vuex";
 export default {
   name: "JobPostingsCards",
+  props: ["jobPosting", "index", "selected"]
 };
 </script>
