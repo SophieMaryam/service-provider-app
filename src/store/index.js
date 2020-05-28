68c77983ed4937893fe7e51b2ff91ec759036d3f
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 10:25:09 +0200

    Add userDetails to store
    vuex-persistedstate to persist the data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    userName: "",
+    userAge: ""
+  },
+  mutations: {
+    UPDATE_USER_DATA(state, payload) {
+      const { userName, userAge } = payload.userDetails;
+      state.userAge = userAge;
+      state.userName = userName;
+    }
+  },
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage
+    })
+  ]
 });
