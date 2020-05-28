c973aaabf40b102f3733ae06a01ab9237e1fab6d
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 15:42:57 +0200

    Add JobPostingCards component  + submit button

--- src/views/JobPostings.vue
+++ src/views/JobPostings.vue
@@ -4,18 +4,32 @@
       Fill in your settings to get client requests!
     </div>
     <div v-else>
-
+      <JobPostingCards
+      />
+      <b-button
+        class="mb-4 colour"
+        variant="primary"
+        @click.prevent="onSubmit()"
+        >Submit Request</b-button
+      >
     </div>
   </div>
 </template>
 <script>
+import JobPostingCards from "../components/JobPostingCards.vue";
 import { mapState } from "vuex";
 export default {
   name: "JobPostings",
+  components: {
+    JobPostingCards
+  },
   computed: {
     ...mapState(["displayClients"])
   },
   methods: {
+    onSubmit() {
+      console.log("test")
+    }
   }
 };
 </script>
