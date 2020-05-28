d8a03aae6540bfc10b1cb478b328f09e59ea3c17
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 10:03:50 +0200

    Create SettingsPage with input fields

---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Update Your User Details and Skills:</h1>
+    <b-form @submit.prevent="onSubmit(userDetails)" class="text-left">
+      <b-form-group class="width" label="Your Name:" label-for="input-1">
+        <b-form-input
+          id="userName"
+          v-model="userDetails.userName"
+          type="text"
+          placeholder="Enter Name"
+        />
+      </b-form-group>
+      <b-form-group class="width" label="Your Age:" label-for="input-2">
+        <b-form-input
+          id="userAge"
+          v-model="userDetails.userAge"
+          placeholder="Enter Age"
+        />
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  name: "SettingsPage",
+  data() {
+    return {
+      userDetails: {
+        userName: "",
+        userAge: null,
+      },
+    }
+  },
+  methods: {
+    onSubmit(userDetails) {
+      console.log(userDetails)
+    }
+  }
+};
+</script>
+
+<style>
+.width {
+  width: 500px;
+}
+</style>
