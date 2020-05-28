7ac7883ead1bbc33f10568766f2ec426ee7e38ac
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 15:42:07 +0200

    Get displayClients from store

--- src/views/JobPostings.vue
+++ src/views/JobPostings.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="container">
+    <div v-if="!displayClients">
+      Fill in your settings to get client requests!
+    </div>
+    <div v-else>
+
+    </div>
   </div>
 </template>
 <script>
+import { mapState } from "vuex";
 export default {
   name: "JobPostings",
+  computed: {
+    ...mapState(["displayClients"])
+  },
+  methods: {
+  }
 };
 </script>
 
