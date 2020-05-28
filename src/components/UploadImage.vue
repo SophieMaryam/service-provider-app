b88ddb05ef9e0b46158659b8054c9ce94fea308a
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 11:10:52 +0200

    Create UploadImage using npm package

---
+++ src/components/UploadImage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <picture-input
+      class="display"
+      ref="pictureInput"
+      @change="onChanged"
+      @remove="onRemoved"
+      :width="150"
+      :removable="true"
+      removeButtonClass="ui"
+      :height="150"
+      accept="image/jpeg, image/png, image/gif"
+      buttonClass="ui"
+    ></picture-input>
+  </div>
+</template>
+<script>
+import PictureInput from "vue-picture-input";
+
+export default {
+  name: "UploadImage",
+  components: {
+    PictureInput
+  },
+  data() {
+    return {
+      image: ""
+    };
+  },
+  methods: {
+    onChanged() {
+      if (!this.$refs.pictureInput.file) {
+         throw new Error("Old browser. No support for Filereader API");
+      } 
+      return this.image = this.$refs.pictureInput.file;
+    },
+    onRemoved() {
+      this.image = "";
+    },
+    attemptUpload(image) {
+      if (!image) {
+        throw new Error("FileReader API not supported");
+      } 
+      return this.image = image;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.display {
+  display: inline-flex;
+}
+
+</style>
