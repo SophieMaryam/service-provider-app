ec2272f5bf868c14a15d24c17bf4d46c06b35972
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 14:15:08 +0200

    Create ClientReqComplete.vue

---
+++ src/views/ClientReqComplete.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h1>Your requests have been sent! You will hear from them shortly.</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "ClientReqComplete"
+};
+</script>
