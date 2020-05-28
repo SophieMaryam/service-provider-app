b0fda9ce93924abbf4e9150b05de8bb61061d2c8
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 14:10:21 +0200

    Add new value to state

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,8 @@ export default new Vuex.Store({
   state: {
     userName: "",
     userAge: "",
-    skills: {}
+    skills: {},
+    displayClients: false
   },
   mutations: {
     UPDATE_USER_DATA(state, payload) {
