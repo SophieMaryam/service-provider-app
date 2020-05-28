675ff313be12e90a9f23ee9eb5eaf1f7411f6524
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 15:52:07 +0200

    Retrieve skills and randomise display

--- src/components/JobPostingCards.vue
+++ src/components/JobPostingCards.vue
@@ -8,6 +8,9 @@
           <span class="font-weight-bold">End Date:</span>
           {{ jobPosting.endDate }}
         </b-card-text>
+        <b-card-text
+          >Skills: {{ name }} | Level: {{ clientLevelRequired }}
+        </b-card-text>
       </b-card>
     </b-card-group>
   </div>
@@ -16,6 +19,28 @@
 import { mapState } from "vuex";
 export default {
   name: "JobPostingsCards",
-  props: ["jobPosting", "index", "selected"]
+  props: ["jobPosting", "index", "selected"],
+  data() {
+    return {
+      name: "",
+      levels: [],
+      clientLevelRequired: 10,
+    };
+  },
+  computed: {
+    ...mapState(["skills"])
+  },
+  mounted() {
+    this.setSelectedSkills();
+  },
+  methods: {
+    setSelectedSkills() {
+      const selectedSkills = this.skills;
+      const selectAtRandom =
+        selectedSkills[Math.floor(Math.random() * selectedSkills.length)];
+      this.name = selectAtRandom.name;
+      this.setLevel(selectAtRandom.level);
+    }
+  }
 };
 </script>
