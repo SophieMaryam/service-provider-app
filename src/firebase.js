789b045c88e3f04f6b7baa722b59628ac475d7fb
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-28 11:09:53 +0200

    Temorarily comment out until further use

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/firebase-auth";
-import "firebase/storage";
-import { firebaseConfig } from "./common/config.js";
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+// import "firebase/firebase-auth";
+// import "firebase/storage";
+// import { firebaseConfig } from "./common/config.js";
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-export const db = firebase.firestore();
+// export const db = firebase.firestore();
 
-export async function getAllJobPostings() {
-  const querySnapshot = await db.collection("JobPostings").get();
-  return querySnapshot.docs.map(doc => doc.data());
-}
+// export async function getAllJobPostings() {
+//   const querySnapshot = await db.collection("JobPostings").get();
+//   return querySnapshot.docs.map(doc => doc.data());
+// }
