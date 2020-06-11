671b055854ad82fa48c61065cb5511923921ec5a
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-06-11 10:37:39 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import "firebase/firestore";
 import "firebase/firebase-auth";
 import "firebase/storage";
 import { firebaseConfig } from "./common/config.js";
+import store from "./store/index"
 
 firebase.initializeApp(firebaseConfig);
 
@@ -10,5 +11,19 @@ export const db = firebase.firestore();
 
 export async function getAllJobPostings() {
   const querySnapshot = await db.collection("JobPostings").get();
-  return querySnapshot.docs.map(doc => doc.data());
+  return querySnapshot.docs.map((doc) => doc.data());
+}
+
+export async function uploadImage(image) {
+  const fileName = image.name
+  const storageRef = firebase.storage().ref('images/'+fileName)
+
+  await storageRef.put(image)
+    .then((snapshot) => {
+      return snapshot.ref.getDownloadURL()
+    }).then((imageUrl) => {
+      store.commit('UPLOAD_IMAGE', imageUrl)
+    }).catch((error) => {
+      console.log(error)
+    })
 }
